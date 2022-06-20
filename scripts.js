169c11e7d013babdd2daa083720fbb1d783eca78
Evelyn Oviangbede <103006429+EvelynOvi@users.noreply.github.com>
2022-06-20 18:10:40 +0100

    update

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const my_name = "Evelyn Oviangbede"
+const my_height = "167cm"
+const my_country = "Nigeria"
+
+document.write("Hi, my name is " + my_name + ', ' + "my height is " + my_height + " and " + "I am from " + my_country + '.')
